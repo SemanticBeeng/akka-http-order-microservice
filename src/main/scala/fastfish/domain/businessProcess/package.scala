af03c8c04380d1441fd4b70b826177fb581d30c0
semanticbeeng <semanticbeeng@email.com>
2016-09-15 09:23:56 +0000

    (wip) FastFish micro service design demo

--- src/main/scala/fastfish/domain/businessProcess/package.scala
+++ src/main/scala/fastfish/domain/businessProcess/package.scala
@@ -9,7 +9,6 @@ import fastfish.domain.catalogMgmt._
 import fastfish.domain.customerMgmt._
 import fastfish.domain.inventoryMgmt._
 
-
 /**
   *
   */
@@ -38,13 +37,17 @@ package object businessProcess {
   trait ShoppingProcess extends BusinessProcess {
 
     def startOrderFor(customer: RegisteredCustomer): Future[Either[OrderInProgressException, Order]]
+
     def shopProducts(order: Order, items: List[(Product, ProductQty)]) : Future[Order]
-    def recommendRelatedFor(order: Order): Future[List[Product]]
-    def checkoutOrder(order: Order, billTo: BillingTo, shipTo: ShippingTo) : Future[Either[NonEmptyList[OrderCheckoutError], Order]]
+
+    def recommendRelatedFor(order: Order) : Future[List[Product]]
+
+    def checkoutOrder(order: Order, billTo: BillingTo, shipTo: ShippingTo)
+    : Future[Either[NonEmptyList[OrderCheckoutError], Order]]
+
     def expireOrder(order: Order) : Future[Order]
   }
 
   trait OrderInProgressException extends BusinessException
   trait OrderCheckoutError extends BusinessException
-
 }
